f44fdb6a39ee54f3e693bd00cf0c377924840dff
mohnoor94 <moh.noor94@gmail.com>
2020-11-23 23:35:45 +0200

    add 26: scripts

---
+++ 26 - Lecture 17/script_02/calculator.py
@@ -0,0 +1,13 @@
+print('Hello from my Python Calculator:')
+
+num1 = float(input('Enter any number 🤔 '))
+num2 = float(input('Enter another number 😠 '))
+
+print('👇🏼👇🏼👇🏼👇🏼👇🏼👇🏼👇🏼👇🏼👇🏼👇🏼👇🏼👇🏼')
+print(f'😌 {num1} + {num2}  = {num1 + num2}')
+print(f'😏 {num1} - {num2}  = {num1 - num2}')
+print(f'🥱 {num1} * {num2}  = {num1 * num2}')
+print(f'😤 {num1} / {num2}  = {num1 / num2}')
+print(f'😨 {num1} // {num2} = {num1 // num2}')
+print(f'🤡 {num1} % {num2}  = {num1 % num2}')
+print('👆🏼👆🏼👆🏼👆🏼👆🏼👆🏼👆🏼👆🏼👆🏼👆🏼👆🏼👆🏼')
---
+++ 26 - Lecture 17/script_03/instant_calcolator.py
@@ -0,0 +1,17 @@
+import sys
+
+arguments = sys.argv 
+print(f"All args: {arguments}")
+
+num1 = float(arguments[1])
+num2 = float(arguments[2])
+
+
+print('👇🏼👇🏼👇🏼👇🏼👇🏼👇🏼👇🏼👇🏼👇🏼👇🏼👇🏼👇🏼')
+print(f'😌 {num1} + {num2}  = {num1 + num2}')
+print(f'😏 {num1} - {num2}  = {num1 - num2}')
+print(f'🥱 {num1} * {num2}  = {num1 * num2}')
+print(f'😤 {num1} / {num2}  = {num1 / num2}')
+print(f'😨 {num1} // {num2} = {num1 // num2}')
+print(f'🤡 {num1} % {num2}  = {num1 % num2}')
+print('👆🏼👆🏼👆🏼👆🏼👆🏼👆🏼👆🏼👆🏼👆🏼👆🏼👆🏼👆🏼')
